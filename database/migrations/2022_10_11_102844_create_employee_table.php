9cb2406bb8e051c89316e602eab64a14c1b84445
Avhishek <avhishek@canvascraft.media>
2022-10-12 18:24:01 +0530

    ADded employee controller

---
+++ database/migrations/2022_10_11_102844_create_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('profile');
+            $table->unsignedBigInteger('employee_id'); //making foreign key
+            $table->foreign('employee_id')->references('id')->on('users')->default()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
