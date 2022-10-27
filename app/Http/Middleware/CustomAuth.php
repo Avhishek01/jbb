6a7c0f42c41297869aeee7479f6de1b17db3d4e2
Avhishek <avhishek@canvascraft.media>
2022-10-27 18:44:40 +0530

    customauth

--- app/Http/Middleware/CustomAuth.php
+++ app/Http/Middleware/CustomAuth.php
@@ -5,20 +5,26 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Session;
+use App\Models\employe;
 use Illuminate\Support\Facades\Auth;
 
 class CustomAuth
 {
 
     public function handle(Request $request, Closure $next)
-    {
-        if (Auth::check()) {
-            // The user is logged in...
-            return $next($request);
-        }
-       else
-       {
-            return redirect('login');
-       }
+    {   
+
+        //dd(Auth::user());
+            $Name = Auth::user()->name;
+            
+            $firstchar = substr($Name,0,1);
+            //dd($firstchar);
+            if($firstchar == 'A' && auth::check()){
+                return $next($request);
+            }else{
+                return redirect('/dashboard');
+            }
+        
     }
-}
+    }
+
